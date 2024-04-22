f182429490a6128f9482d67b849dcfeddc4b4e4f
Wongvaritanprasertporn <71322329+Wongvaritanprasertporn@users.noreply.github.com>
2024-04-22 12:53:49 +0000

    Intermission #1 22/4/2024

--- reversi.js
+++ reversi.js
@@ -1,131 +1,116 @@
 class Reversi {
-    constructor() {
-        this.currentPlayer = Math.random() < 0.5 ? "black" : "white"
-        this.mat = new Array;
-        this.isGameOver == false;
+  constructor() {
+    this.currentPlayer = Math.random() < 0.5 ? "black" : "white";
+    this.mat = new Array();
+    this.isGameOver == false;
 
-        for (let i = 0; i < 8; i++) {
-            let col = new Array;
-            for (let j = 0; j < 8; j++) {
-                if ((i == 3 && j == 3) || (i == 4 && j == 4)) {
-                    col.push('black')
-                }
-                else if ((i == 3 && j == 4) || (i == 4 && j == 3)) {
-                    col.push('white')
-                }
-                else {
-                    col.push(null);
-                }
-            }
-            this.mat.push(col)
+    for (let i = 0; i < 8; i++) {
+      let col = new Array();
+      for (let j = 0; j < 8; j++) {
+        if ((i == 3 && j == 3) || (i == 4 && j == 4)) {
+          col.push("black");
+        } else if ((i == 3 && j == 4) || (i == 4 && j == 3)) {
+          col.push("white");
+        } else {
+          col.push(null);
         }
+      }
+      this.mat.push(col);
+    }
+  }
+
+  getCurrentPcs() {
+    let white, black;
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        this.mat[i][j] === "black" ? black++ : white++;
+      }
+    }
+    return { white, black };
+  }
+
+  // Function to check if a move is valid
+  isValidMove(row, col) {
+    // Check if the cell is empty
+    if (this.mat[row][col] !== null) {
+      return false;
     }
 
-    getCurrentPcs() {
-        let white, black;
-        for (let i = 0; i < 8; i++) {
-            for (let j = 0; j < 8; j++) {
-                this.mat[i][j] === "black" ? black++ : white++;
-            }
+    // Check if the move flips opponent discs in any direction
+    let valid = [];
+    for (let dr = -1; dr <= 1; dr++) {
+      for (let dc = -1; dc <= 1; dc++) {
+        if (dr === 0 && dc === 0) continue; // Skip the current cell
+        let flipDiscPos = this.flipsOpponentDiscs(row, col, dr, dc);
+        if (flipDiscPos !== false) {
+          for (let disc in flipDiscPos) {
+            valid.push(disc);
+          }
         }
-        return {white, black}
+      }
     }
+    return valid;
+  }
 
-    getMat() {
-        return this.mat
-    }
+  // Function to handle user move and update the board
+  handleMove(row, col) {
+    console.log("handleMove");
+    // Implement the game logic here, updating the board and checking for valid moves and flips
+    let discsToFlip = this.isValidMove(row, col);
+    if (discsToFlip.length !== 0 && discsToFlip !== false) {
+      // Place the current player's disc
+      this.mat[row][col] = this.currentPlayer;
+      console.log(discsToFlip);
+      for (let disc in discsToFlip) {
+        this.mat[disc.row][disc.col] = this.currentPlayer;
+      }
 
-    getTurn() {
-        return this.currentPlayer
-    }
+      // Switch to the other player's turn
+      this.currentPlayer = this.currentPlayer === "black" ? "white" : "black";
 
-    isGameOver() {
-        return this.isGameOver
-    }
-
-    // Function to handle user move and update the board
-    handleMove(row, col) {
-        // Implement the game logic here, updating the board and checking for valid moves and flips
-        let discsToFlip = isValidMove(row, col)
-        if (discsToFlip.length !== 0 && discsToFlip !== false) {
-            // Place the current player's disc
-            this.mat[row][col] = this.currentPlayer;
-
-            for (let disc in discsToFlip) {
-                const { row, col } = disc;
-                this.mat[row][col] = this.currentPlayer;
-            }
-
-            // Switch to the other player's turn
-            this.currentPlayer = this.currentPlayer === 'black' ? 'white' : 'black';
-
-            if (isGameOver()) {
-                this.isGameOver == true
-            }
-
+      let flag = true;
+      for (let row = 0; row < 8; row++) {
+        for (let col = 0; col < 8; col++) {
+          if (this.isValidMove(row, col)) {
+            flag = false;
+          }
         }
+      }
+      if (flag) {
+        this.isGameOver == true;
+      }
+    }
+  }
+
+  flipsOpponentDiscs(row, col, dr, dc) {
+    const opponent = this.currentPlayer === "black" ? "white" : "black";
+    let r = row + dr;
+    let c = col + dc;
+    let discsToFlip = [];
+
+    while (this.isValidCell(r, c) && this.mat[r][c] !== null) {
+      if (this.mat[r][c] === opponent) {
+        discsToFlip.push({ row: r, col: c });
+        r += dr;
+        c += dc;
+      } else {
+        break;
+      }
     }
 
-    // Function to check if a move is valid
-    isValidMove(row, col) {
-        // Check if the cell is empty
-        if (this.mat[i][j] !== undefined) {
-            return false;
-        }
-
-        // Check if the move flips opponent discs in any direction
-        let valid = []
-        for (let dr = -1; dr <= 1; dr++) {
-            for (let dc = -1; dc <= 1; dc++) {
-                if (dr === 0 && dc === 0) continue; // Skip the current cell
-                let flipDiscPos = flipsOpponentDiscs(row, col, dr, dc)
-                if (flipDiscPos !== false) {
-                    flipDiscPos.forEach(disc => {
-                        valid.push(disc);
-                    })
-                }
-            }
-        }
-        return valid;
+    if (
+      this.isValidCell(r, c) &&
+      discsToFlip.length > 0 &&
+      this.mat[r][c] === this.currentPlayer
+    ) {
+      return discsToFlip;
     }
+    return false;
+  }
 
-    flipsOpponentDiscs(row, col, dr, dc) {
-        const opponent = this.currentPlayer === 'black' ? 'white' : 'black';
-        let r = row + dr;
-        let c = col + dc;
-        let discsToFlip = [];
-
-        while (isValidCell(r, c) && this.mat[r][c] !== undefined) {
-            if (this.mat[r][c] === opponent) {
-                discsToFlip.push({ row: r, col: c });
-                r += dr;
-                c += dc;
-            } else { break }
-        }
-
-        if (isValidCell(r, c) && this.mat[r][c] !== undefined && discsToFlip.length > 0) {
-            if (this.mat[r][c] === this.currentPlayer) {
-                return discsToFlip;
-            }
-        }
-        return false;
-    }
-
-    isValidCell(row, col) {
-        return row >= 0 && row < 8 && col >= 0 && col < 8;
-    }
-
-    isGameOver() {
-        for (let row = 0; row < 8; row++) {
-            for (let col = 0; col < 8; col++) {
-                if (isValidMove(row, col) && this.mat[row][col] === undefined) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+  isValidCell(row, col) {
+    return row >= 0 && row < 8 && col >= 0 && col < 8;
+  }
 }
 
 module.exports = Reversi;
-
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const io = require("socket.io")(server);
 const { v4: uuidv4 } = require("uuid");
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/"));
 
 const matchmakingQueue = [];
 const onlineRooms = [];
@@ -16,40 +16,47 @@ io.on("connection", (socket) => {
   let room_uuid;
   let game;
 
+  socket.on("checkRoom", () => {
+    let room = onlineRooms.find(
+      (item) =>
+        item.player1 === socket.handshake.address ||
+        item.player2 === socket.handshake.address,
+    );
+    if (room != undefined) {
+      socket.emit("roomFound", room.room_uuid);
+    }
+  });
+
   socket.on("join", () => {
-    matchmakingQueue.push(socket);
-    if (matchmakingQueue.length >= 2) {
-      let player1 = matchmakingQueue.shift();
-      let player2 = matchmakingQueue.shift();
-      room_uuid = uuidv4();
-      onlineRooms.push({
-        room_uuid: room_uuid,
-        reversi: new Reversi(),
-        player: [player1, player2],
-      });
-      player1.join(room_uuid);
-      player2.join(room_uuid);
-      game = onlineRooms.find((item) => item.room_uuid === room_uuid);
-      setTimeout(() => {
-        io.to(room_uuid).emit("matched", "/game/" + room_uuid);
-      }, 3000);
+    let room = onlineRooms.find(
+      (item) =>
+        item.player1 === socket.handshake.address ||
+        item.player2 === socket.handshake.address,
+    );
+    if (room !== undefined) {
+      room_uuid = room.room_uuid;
+      socket.join(room_uuid);
+      game = room.reversi;
+      if (room.player1 === socket.handshake.address) {
+        color = "white";
+      } else {
+        color = "black";
+      }
+      io.to(room_uuid).emit("updateGame", game.mat, game.currentPlayer);
     }
   });
 
   socket.on("button_pressed", (row, col) => {
-    console.log(game);
-    if (color == game.currentPlayer) {
+    if (color === game.currentPlayer) {
       game.handleMove(row, col);
-      socket.to(room_uuid).emit("updateGame", game.getMat(), game.getTurn());
-      if (game.isGameOver()) {
-        socket.to(room_uuid).emit("gameover");
+      io.to(room_uuid).emit("updateGame", game.mat, game.currentPlayer);
+      if (game.isGameOver) {
+        io.to(room_uuid).emit("gameover");
       }
-      socket.emit("turn_info", game.currentPlayer);
     }
   });
 
   socket.on("disconnect", async () => {
-    console.log("leave");
     socket.emit("player_leave");
     setTimeout(() => {
       let index = -1;
@@ -59,7 +66,6 @@ io.on("connection", (socket) => {
           break;
         }
       }
-
       if (index !== -1) {
         onlineRooms.splice(index, 1);
       }
@@ -68,42 +74,37 @@ io.on("connection", (socket) => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/lobby.html");
+  matchmakingQueue.push(req.ip);
+  res.sendFile("lobby.html", { root: __dirname });
+  if (matchmakingQueue.length >= 2) {
+    let player1 = matchmakingQueue.shift();
+    let player2 = matchmakingQueue.shift();
+    const room_uuid = uuidv4();
+    onlineRooms.push({
+      room_uuid: room_uuid,
+      reversi: new Reversi(),
+      player1: player1,
+      player2: player2,
+    });
+  }
 });
 
 app.get("/game/:roomId", (req, res) => {
-  const room_uuid = req.params.roomId;
-  const room = onlineRooms.find((item) => item.room_uuid === room_uuid);
-  let game;
-  if (
-    onlineRooms.includes(room) &&
-    (room.player[0].handshake.address === req.ip ||
-      room.player[1].handshake.address === req.ip)
-  ) {
-    res.sendFile(__dirname + "/public/reversi.html");
-    game = room.reversi;
-    color = game.currentPlayer;
-    io.to(room_uuid).emit("turn_info", game.currentPlayer);
+  const room = onlineRooms.find((item) => item.room_uuid === req.params.roomId);
+  if (room.player1 === req.ip || room.player2 === req.ip) {
+    res.sendFile(__dirname + "/reversi.html");
   } else {
     res.status(404).send("Room not found");
   }
 });
 
 app.get("/result/:roomId", (req, res) => {
-  let room_uuid;
-  if (
-    onlineRooms.has(room_uuid) &&
-    (room.player[0].handshake.address === req.ip ||
-      room.player[1].handshake.address === req.ip)
-  ) {
-    room_uuid = req.params.roomId;
-    let game = onlineRooms.get(room_uuid);
-    if (game.reversi.isGameOver()) {
+  const room = onlineRooms.find((item) => item.room_uuid === req.params.roomId);
+  if (room.player1 === req.ip || room.player2 === req.ip) {
+    if (room.reversi.isGameOver) {
       res.send(game.reversi.getCurrentPcs);
     }
-    game.player[0].leave();
-    game.player[1].leave();
-    onlineRooms.delete(room_uuid);
+    onlineRooms.delete(room);
   }
 });
 
